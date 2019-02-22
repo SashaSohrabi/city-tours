f59e4cb920ee4c0f2929c4c6c7d438e1d7f086fc
sashasohrabi <shb.sohrabi@gmail.com>
2019-02-23 01:29:24 +0200

    TourList component created.

---
+++ src/components/TourList/TourList.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./TourList.scss";
+
+class TourList extends Component {
+  render() {
+    return <section>tour</section>;
+  }
+}
+
+export default TourList;
