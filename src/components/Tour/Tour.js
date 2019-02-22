f4a2b56816a5b831fdfb92c9388377d347279170
sashasohrabi <shb.sohrabi@gmail.com>
2019-02-23 01:41:13 +0200

    created Tour component and imported into TourList component.

---
+++ src/components/Tour/Tour.js
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./Tour.scss";
+
+class Tour extends Component {
+  render() {
+    return <div>single tour</div>;
+  }
+}
+
+export default Tour;
--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -1,9 +1,14 @@
 import React, { Component } from "react";
 import "./TourList.scss";
+import Tour from "../Tour/Tour";
 
 class TourList extends Component {
   render() {
-    return <section>tour</section>;
+    return (
+      <section>
+        <Tour />
+      </section>
+    );
   }
 }
 
