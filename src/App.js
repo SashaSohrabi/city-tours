bce3e2d2a25563813e2d31ef929f4b134ee5f920
sashasohrabi <shb.sohrabi@gmail.com>
2019-02-23 00:45:49 +0200

    created Navbar component.

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React, { Component } from "react";
 import "./App.scss";
+import Navbar from "./components/Navbar/Navbar";
 
 class App extends Component {
   render() {
-    return <div>Hello</div>;
+    return <Navbar />;
   }
 }
 
