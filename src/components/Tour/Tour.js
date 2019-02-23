70adb349c7f7998573027317b38acd74849fcb98
sashasohrabi <shb.sohrabi@gmail.com>
2019-02-23 14:42:35 +0200

    importing tourData to tourlist and passing through props to tour component.

--- src/components/Tour/Tour.js
+++ src/components/Tour/Tour.js
@@ -3,24 +3,25 @@ import "./Tour.scss";
 
 class Tour extends Component {
   render() {
+    const { city, img, name, info } = this.props.tour;
     return (
       <article className="tour">
         <div className="img-container">
-          <img src="https://images.pexels.com/photos/17729/pexels-photo.jpg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940" />
+          <img src={img} alt="tour-item" />
           <span className="close-btn">
             <i className="fas fa-window-close" />
           </span>
         </div>
         <div className="tour-info">
-          <h3>city</h3>
-          <h4>name</h4>
+          <h3>{city}</h3>
+          <h4>{name}</h4>
           <h5>
-            info{""}
+            info{" "}
             <span>
               <i className="fas fa-caret-square-down" />
             </span>
           </h5>
-          <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit.</p>
+          <p>{info}</p>
         </div>
       </article>
     );
--- src/components/TourList/TourList.js
+++ src/components/TourList/TourList.js
@@ -1,15 +1,22 @@
 import React, { Component } from "react";
 import "./TourList.scss";
 import Tour from "../Tour/Tour";
+import { tourData } from "../tourData";
 
 class TourList extends Component {
   state = {
-    tours: []
+    tours: tourData
   };
+
   render() {
+    const { tours } = this.state;
+
+    console.log(this.state.tours);
     return (
       <section className="tour-list">
-        <Tour />
+        {tours.map(tour => {
+          return <Tour key={tour.id} tour={tour} />;
+        })}
       </section>
     );
   }
